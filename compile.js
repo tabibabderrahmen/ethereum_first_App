1e1456fd16049a458ba2d6dcc07cc841a46500b2
tabibabderrahmen <abdou1tabib@gmail.com>
2022-06-05 19:19:15 +0100

    Adding files

---
+++ compile.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const fs = require('fs');
+const solc = require('solc');
+
+const employeePath = path.resolve(__dirname, 'contracts', 'Employee.sol');
+const source = fs.readFileSync(employeePath, 'utf8');
+
+const input = {
+  language: 'Solidity',
+  sources: {
+    'Employee.sol': {
+      content: source,
+    },
+  },
+  settings: {
+    outputSelection: {
+      '*': {
+        '*': ['*'],
+      },
+    },
+  },
+};
+
+module.exports = JSON.parse(solc.compile(JSON.stringify(input))).contracts[
+  'Employee.sol'
+].Employee;
